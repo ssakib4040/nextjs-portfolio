d4b5d835b754585de3650177e7b466cad35673cb
Sadman Sakib <ssakib4040@gmail.com>
2022-06-18 19:14:36 +0600

    init

---
+++ src/App.jsx
@@ -0,0 +1,264 @@
+import React, { useEffect, useRef } from "react";
+import "./App.css";
+
+function App() {
+  const sidebar = useRef(null);
+  const button = useRef(null);
+  const mainContainer = useRef(null);
+
+  // handle sidebar
+  const handleSidebar = (status) => {
+    if (typeof status == "boolean") {
+      if (status) {
+        sidebar.current.classList.remove("active");
+        button.current.style.left = "300px";
+        mainContainer.current.style.marginLeft = "300px";
+      } else {
+        sidebar.current.classList.add("active");
+        button.current.style.left = "0px";
+        mainContainer.current.style.marginLeft = "0px";
+      }
+      return;
+    }
+
+    if (sidebar.current.classList.contains("active")) {
+      sidebar.current.classList.remove("active");
+      button.current.style.left = "300px";
+      mainContainer.current.style.marginLeft = "300px";
+    } else {
+      sidebar.current.classList.add("active");
+      button.current.style.left = "0px";
+      mainContainer.current.style.marginLeft = "initial";
+    }
+  };
+
+  // sidebar resize
+  useEffect(() => {
+    function checkSidebar() {
+      if (window.screen.width > 786) {
+        handleSidebar(true);
+      } else {
+        handleSidebar(false);
+      }
+    }
+    checkSidebar();
+
+    window.addEventListener("resize", function (event) {
+      checkSidebar();
+    });
+  }, []);
+
+  return (
+    <div className="container-fluid ">
+      {/* sidebar button  */}
+      <button
+        className="navbar-button btn btn-primary"
+        ref={button}
+        onClick={handleSidebar}
+      >
+        <span className="navbar-toggler-icon"></span>
+      </button>
+
+      {/* sidebar  */}
+      <div className="sidebar  border active" ref={sidebar}>
+        <div className="sidebar__top ">
+          <div className="sidebar__top_img_wrap">
+            <img
+              src="https://firebasestorage.googleapis.com/v0/b/production-portfolio-9a395.appspot.com/o/29249326_548922835491216_4695470664184758272_n.webp?alt=media&token=d3a53c04-3d68-41bd-b3a8-3e390121e8ac"
+              className="img-fluid sidebar__img"
+              alt=""
+            />
+          </div>
+        </div>
+        <hr className="border border-dark" />
+        <div className="sidebar__middle ">
+          <ul>
+            <li>
+              <a href="#home">Home</a>
+            </li>
+
+            <li>
+              <a href="#resume">Resume</a>
+            </li>
+            <li>
+              <a href="#portfolio">Portfolio</a>
+            </li>
+
+            <li>
+              <a href="#contact">Contact</a>
+            </li>
+          </ul>
+        </div>
+        <hr className="border border-dark" />
+        <div className="sidebar__bottom mb-3 fw-bolder">
+          &copy;Copyright 2022. All Rights reserved
+        </div>
+      </div>
+
+      {/* main content */}
+      <div className="content " ref={mainContainer}>
+        {/* home  */}
+        <div className="home" id="home">
+          <div className="text-center">
+            <h2 className=" fw-bold fs-1">
+              My name is <span className="text-primary">Sadman Sakib</span>
+            </h2>
+            <p className="fs-4  text-secondary">
+              I am a full stack web developer
+            </p>
+          </div>
+        </div>
+
+        {/* resume  */}
+        <div className="resume" id="resume">
+          <h2>Resume</h2>
+          <hr />
+          <p>Hi, My name is Sadman Sakib. I am a full stack web developer.</p>
+
+          <p>
+            I am looking for Intern/Junior Job(remote). I am still learning web
+            development & I want to take my career to next level & improve my
+            skills with the Junior position jobs/internship.
+          </p>
+
+          <div>
+            Some of my skills are:
+            <ul>
+              <li>Html</li>
+              <li>CSS</li>
+              <li>Javascript</li>
+              <li>Jquery</li>
+              <li>Bootstrap</li>
+              <li>NodeJS</li>
+              <li>Express</li>
+              <li>React</li>
+              <li>Redux</li>
+              <li>MongoDB</li>
+            </ul>
+          </div>
+
+          <p>Please hire me If you like my portfolio</p>
+          <p className="fw-bold">
+            [ Note: I didn't used any fancy css animations because most of the
+            time HR avoid such website ]
+          </p>
+        </div>
+
+        {/* portfolio  */}
+        <div className="portfolio mt-5" id="portfolio">
+          <h2>Portfolio</h2>
+          <div className="container">
+            <div className="row">
+              <Portfolio
+                image="https://firebasestorage.googleapis.com/v0/b/production-portfolio-9a395.appspot.com/o/authentication.png?alt=media&token=954688ca-1a5b-44f4-8a13-d80c741255fe"
+                name="Mern Authentication"
+                projectUrl="https://mern-authentication-ssakib4040.herokuapp.com/"
+              />
+
+              <Portfolio
+                image="https://firebasestorage.googleapis.com/v0/b/production-portfolio-9a395.appspot.com/o/authentication-2.0.png?alt=media&token=7951396f-2e52-471c-9bc2-82710af3b8bf"
+                name="Mern Authentication 2.0"
+                projectUrl="https://mern-authentication-2.herokuapp.com/"
+              />
+
+              <Portfolio
+                image="https://firebasestorage.googleapis.com/v0/b/production-portfolio-9a395.appspot.com/o/ecommerce.png?alt=media&token=00b332d6-9e47-4ab3-b655-21fe433bec79"
+                name="Mern Ecommerce"
+                projectUrl="https://mern-ecommerce-ssakib4040.herokuapp.com/"
+              />
+
+              <Portfolio
+                image="https://firebasestorage.googleapis.com/v0/b/production-portfolio-9a395.appspot.com/o/mern-todo-app.png?alt=media&token=b4fbcbad-1868-4dbb-8b51-fef66c7640e3"
+                name="Mern Todo App"
+                projectUrl="https://mern-todo-app-ssakib4040.herokuapp.com/"
+              />
+
+              <Portfolio
+                image="https://firebasestorage.googleapis.com/v0/b/production-portfolio-9a395.appspot.com/o/tic-tac-toe.png?alt=media&token=8a167ee3-af77-47d0-8a1e-f8bc2876c952"
+                name="React Tic Tac Toe"
+                projectUrl="https://ssakib4040.github.io/react-tic-tac-toe/"
+              />
+            </div>
+          </div>
+          <hr />
+        </div>
+
+        {/* npm package */}
+        <div className="portfolio mt-5" id="portfolio">
+          <h2>NPM Package</h2>
+          <div className="container">
+            <div className="row">
+              <Portfolio
+                image="https://firebasestorage.googleapis.com/v0/b/production-portfolio-9a395.appspot.com/o/npm-logo.webp?alt=media&token=8e1054cb-29cf-4fdb-ab5b-59da586a54c5"
+                name="Mongoose Error Beautify"
+                projectUrl="https://mern-authentication-ssakib4040.herokuapp.com/"
+                btnText="Link"
+              />
+
+              <Portfolio
+                image="https://firebasestorage.googleapis.com/v0/b/production-portfolio-9a395.appspot.com/o/npm-logo.webp?alt=media&token=8e1054cb-29cf-4fdb-ab5b-59da586a54c5"
+                name="Math Dumb"
+                projectUrl="https://mern-authentication-2.herokuapp.com/"
+                btnText="Link"
+              />
+            </div>
+          </div>
+          <hr />
+        </div>
+
+        {/* contact  */}
+        <div className="contact" id="contact">
+          <h2>Contact</h2>
+          <hr />
+
+          <a
+            href="https://github.com/ssakib4040"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <i className="fa-brands fa-github-square"></i>
+          </a>
+
+          <a href="https://fb.com/ssakib4040" target="_blank" rel="noreferrer">
+            <i className="fa-brands fa-facebook"></i>
+          </a>
+
+          <a
+            href="https://twitter.com/ssakib4040"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <i className="fa-brands fa-twitter"></i>
+          </a>
+
+          <a href="mailto:ssakib4040@gmail.com">
+            <i className="fa-solid fa-at"></i>
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function Portfolio(props) {
+  return (
+    <div className="col-xl-3 mb-3">
+      <div className="card ">
+        <img src={props.image} className="card-img-top" alt="" />
+        <div className="card-body">
+          <h5 className="card-title">{props.name}</h5>
+          <a
+            href={props.projectUrl}
+            className="btn btn-primary mt-3"
+            target="_blank"
+            rel="noreferrer"
+          >
+            {props?.btnText ? props.btnText : "Demo"}
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
