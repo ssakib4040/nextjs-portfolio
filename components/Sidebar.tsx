9a86c71da5fe85c5322fa9185fdf2f39488545f2
Sadman Sakib <ssakib4040@gmail.com>
2023-04-19 06:31:55 +0600

    Mobile responsive button added

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,10 +1,17 @@
-import Image from "next/image";
-import Link from "next/link";
-import { useRouter } from "next/router";
 import React from "react";
+import Image from "next/image";
+import { useRouter } from "next/router";
+import { useSidebar } from "@/contexts/Sidebar-ctx";
+
+//
+
+import { FaBars } from "react-icons/fa";
 
 export default function Sidebar({ sidebar }: { sidebar: boolean }) {
   const router = useRouter();
+  const s: any = useSidebar();
+
+  console.log(s);
 
   const sidebarImage = "/profile-image.jpg";
 
@@ -32,44 +39,55 @@ export default function Sidebar({ sidebar }: { sidebar: boolean }) {
   ];
 
   return (
-    <div
-      className={`${
-        sidebar ? "left-0" : "left-[-250px]"
-      } left-0 flex flex-col justify-between py-8 transition-all fixed top-0 bottom-0 w-[250px] bg-[#E6ECF0] overflow-auto border-r-2 border-gray-300`}
-    >
-      <div className="sidebar_top p-8 flex justify-center items-center">
-        <Image
-          className="border-[5px] border-[#0D6EFD] rounded-full p-2"
-          src={sidebarImage}
-          alt="Profile Image"
-          height={200}
-          width={200}
-        />
-      </div>
+    <>
+      <button
+        className={`${
+          s.isOpen ? "left-[250px]" : "left-[0px]"
+        } absolute top-0 bg-[#0D6EFD] text-white px-2 py-1 rounded-sm transition-all`}
+        onClick={() => s.setIsOpen(!s.isOpen)}
+      >
+        <FaBars className="text-2xl" />
+      </button>
 
-      <div className="sidebar_menu">
-        <hr className="my-3 border-t-2 border-gray-300" />
-        <ul>
-          {menus.map((menu, index) => (
-            <li
-              key={index}
-              className={`${
-                router.pathname === menu.path && "bg-[#0D6EFD] text-white"
-              } text-center py-2 text-[#0D6EFD] font-medium hover:bg-[#0D6EFD] hover:text-white transition cursor-pointer`}
-              onClick={() => router.push(menu.path)}
-            >
-              <a>{menu.name}</a>
-            </li>
-          ))}
-        </ul>
-        <hr className="my-3 border-t-2 border-gray-300" />
-      </div>
+      <div
+        className={`${
+          sidebar ? "left-0" : "left-[-250px]"
+        } left-0 flex flex-col justify-between py-8 transition-all fixed top-0 bottom-0 w-[250px] bg-[#E6ECF0] overflow-auto border-r-2 border-gray-300`}
+      >
+        <div className="sidebar_top p-8 flex justify-center items-center">
+          <Image
+            className="border-[5px] border-[#0D6EFD] rounded-full p-2"
+            src={sidebarImage}
+            alt="Profile Image"
+            height={200}
+            width={200}
+          />
+        </div>
 
-      <div className="sidebar_footer">
-        <p className="text-center font-medium">
-          &copy; Copyright 2023. All Rights reserved
-        </p>
+        <div className="sidebar_menu">
+          <hr className="my-3 border-t-2 border-gray-300" />
+          <ul>
+            {menus.map((menu, index) => (
+              <li
+                key={index}
+                className={`${
+                  router.pathname === menu.path && "bg-[#0D6EFD] text-white"
+                } text-center py-2 text-[#0D6EFD] font-medium hover:bg-[#0D6EFD] hover:text-white transition cursor-pointer`}
+                onClick={() => router.push(menu.path)}
+              >
+                <a>{menu.name}</a>
+              </li>
+            ))}
+          </ul>
+          <hr className="my-3 border-t-2 border-gray-300" />
+        </div>
+
+        <div className="sidebar_footer">
+          <p className="text-center font-medium">
+            &copy; Copyright 2023. All Rights reserved
+          </p>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
