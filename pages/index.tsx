cffc8d50c2f7e3fc2b6d0e50f879199d6b8f4c88
Sadman Sakib <ssakib4040@gmail.com>
2023-04-20 18:48:56 +0600

    Index font responsive added

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,10 @@
 export default function Index() {
   return (
     <div className="h-screen flex flex-col justify-center items-center">
-      <h2 className="text-4xl font-medium mb-3">
+      <h2 className="text-3xl md:text-4xl font-medium mb-3">
         My name is <span className="text-[#0D6EFD]">Sadman Sakib</span>
       </h2>
-      <p className="text-2xl font-medium text-gray-500">
+      <p className="text-1xl md:text-2xl font-medium text-gray-500">
         I am a Full Stack Web Developer
       </p>
     </div>
