d1ee84439ef021da61995dc50476c07846cee8f8
Sadman Sakib <ssakib4040@gmail.com>
2023-05-15 12:58:39 +0600

    Typing animaton added in home page job title

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,5 @@
+import { TypeAnimation } from "react-type-animation";
+
 export default function Index() {
   return (
     <div className="h-screen flex flex-col justify-center items-center">
@@ -5,7 +7,25 @@ export default function Index() {
         My name is <span className="text-[#0D6EFD]">Sadman Sakib</span>
       </h2>
       <p className="text-1xl md:text-2xl font-medium text-gray-500">
-        I am a Full Stack Web Developer
+        {/* I am a Full Stack Web Developer */}
+        <TypeAnimation
+          sequence={[
+            // Same String at the start will only be typed once, initially
+            "I am a Full Stack Web Developer",
+            2000,
+            "I am a Web Designer",
+            2000,
+            "I am a Web developer",
+            2000,
+            "I am a Frontend Developer",
+            2000,
+            "I am a Backend Developer",
+            2000,
+          ]}
+          speed={50}
+          // style={{ fontSize: "1em" }}
+          repeat={Infinity}
+        />
       </p>
     </div>
   );
