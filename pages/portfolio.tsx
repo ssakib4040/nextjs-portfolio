75ce295018e1339199185c9add82b35b7351c053
Sadman Sakib <ssakib4040@gmail.com>
2023-05-16 13:36:08 +0600

    githubUrl added in portfolios

--- pages/portfolio.tsx
+++ pages/portfolio.tsx
@@ -13,84 +13,84 @@ export default function Portfolio() {
       title: "Marketing Agency",
       image: "/projects/marketing-house.png",
       link: "https://marketing-house.vercel.app/",
-      githubUrl: "https://github.com/ssakib4040/",
+      githubUrl: "https://github.com/ssakib4040/marketing-house",
     },
 
     {
       title: "React Counter",
       image: "/projects/react-counter.png",
       link: "https://react-counter-zeta.vercel.app/",
-      githubUrl: "https://github.com/ssakib4040/",
+      githubUrl: "https://github.com/ssakib4040/react-counter",
     },
 
     {
       title: "React Todo App",
       image: "/projects/mern-todo-app.png",
       link: "https://mern-todo-slg.netlify.app/",
-      githubUrl: "https://github.com/ssakib4040/",
+      githubUrl: "https://github.com/ssakib4040/react-todo",
     },
 
     {
       title: "React Tic Tac Toe",
       image: "/projects/react-ttt.png",
       link: "https://ssakib4040.github.io/react-tic-tac-toe/",
-      githubUrl: "https://github.com/ssakib4040/",
+      githubUrl: "https://github.com/ssakib4040/react-tic-tac-toe",
     },
 
     {
       title: "React Rock Paper Scissors",
       image: "/projects/react-rock-paper-scissors.png",
       link: "https://react-rock-paper-scissors1.netlify.app/",
-      githubUrl: "https://github.com/ssakib4040/",
+      githubUrl: "https://github.com/ssakib4040/react-rock-paper-scissors",
     },
 
     {
       title: "Nextjs Tailwind Auth",
       image: "/projects/nextjs-tailwind-auth.png",
       link: "https://nextjs-tailwind-auth-lake.vercel.app/",
-      githubUrl: "https://github.com/ssakib4040/",
+      githubUrl: "https://github.com/ssakib4040/nextjs-tailwind-auth",
     },
 
     {
       title: "React Password Generator",
       image: "/projects/react-password-generator.png",
       link: "https://react-password-generator1.netlify.app/",
-      githubUrl: "https://github.com/ssakib4040/",
+      githubUrl: "https://github.com/ssakib4040/react-password-generator",
     },
 
     {
       title: "React Emoji Directory",
       image: "/projects/react-emoji-directory.png",
       link: "https://react-emoji-directory.netlify.app/",
-      githubUrl: "https://github.com/ssakib4040/",
+      githubUrl: "https://github.com/ssakib4040/react-emoji-search",
     },
 
     {
       title: "NextJS Bloghub",
       image: "/projects/nextjs-bloghub.png",
       link: "https://bloghub-red.vercel.app/",
-      githubUrl: "https://github.com/ssakib4040/",
+      githubUrl: "https://github.com/ssakib4040/bloghub",
     },
 
     {
       title: "Mern Authentication",
       image: "/projects/mern-auth.png",
       link: "https://github.com/ssakib4040/mern-authentication",
-      githubUrl: "https://github.com/ssakib4040/",
+      githubUrl: "https://github.com/ssakib4040/mern-authentication",
     },
 
     {
       title: "Mern Authentication 2.0",
       image: "/projects/mern-auth-2.0.png",
       link: "https://github.com/ssakib4040/mern-authentication-2.0",
-      githubUrl: "https://github.com/ssakib4040/",
+      githubUrl: "https://github.com/ssakib4040/mern-authentication-2.0",
     },
 
     {
       title: "Mern Ecommerce",
       image: "/projects/react-ecommerce.png",
       link: "https://github.com/ssakib4040/mern-ecommerce",
-      githubUrl: "https://github.com/ssakib4040/",
+      githubUrl: "https://github.com/ssakib4040/mern-ecommerce",
     },
   ];
 
@@ -103,7 +103,7 @@ export default function Portfolio() {
         {portfolios.map((eachItem, index) => {
           return (
             <div
-              className="flex flex-col rounded-sm border border-gray-300 cursor-pointer"
+              className="flex flex-col rounded-sm border border-gray-300"
               key={index}
             >
               <div
