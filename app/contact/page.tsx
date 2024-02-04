44b4d767a1388970a97937f3e4eb3b98be99bd22
Sadman Sakib <ssakib4040@gmail.com>
2024-02-04 16:19:34 +0600

    Social accounts with valid url added in contact

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,5 +1,62 @@
 import React from "react";
+import { FaGithub, FaFacebook, FaTwitter, FaAt } from "react-icons/fa";
+import { AiFillFileText } from "react-icons/ai";
+
+const urls = {
+  github: "https://github.com/ssakib4040",
+  facebook: "https://www.facebook.com/ssakib4040",
+  twitter: "https://twitter.com/ssakib4040",
+  mailto: "mailto:ssakib4040@gmail.com",
+  cv: "https://drive.google.com/file/d/1ZU5D0XJFrVzwk__eDZZXfg8w0bFxyU5T/view?usp=share_link",
+};
 
 export default function page() {
-  return <div>Resume</div>;
+  return (
+    <div className="h-screen flex justify-center items-center gap-3">
+      <a
+        href={urls.github}
+        target="_blank"
+        title="Github"
+        className="text-[#0D6EFD] cursor-pointer bg-white rounded-md shadow-md p-3"
+      >
+        <FaGithub className="text-6xl" />
+      </a>
+
+      <a
+        className="text-[#0D6EFD] cursor-pointer bg-white rounded-md shadow-md p-3"
+        href={urls.cv}
+        title="CV"
+        target="_blank"
+      >
+        <AiFillFileText className="text-6xl" />
+      </a>
+
+      <a
+        className="text-[#0D6EFD] cursor-pointer bg-white rounded-md shadow-md p-3"
+        href={urls.mailto}
+        title="Email"
+        target="_blank"
+      >
+        <FaAt className="text-6xl" />
+      </a>
+
+      <a
+        className="text-[#0D6EFD] cursor-pointer bg-white rounded-md shadow-md p-3"
+        href={urls.facebook}
+        title="Facebook"
+        target="_blank"
+      >
+        <FaFacebook className="text-6xl" />
+      </a>
+
+      <a
+        className="text-[#0D6EFD] cursor-pointer bg-white rounded-md shadow-md p-3"
+        href={urls.twitter}
+        title="Twitter"
+        target="_blank"
+      >
+        <FaTwitter className="text-6xl" />
+      </a>
+    </div>
+  );
 }
