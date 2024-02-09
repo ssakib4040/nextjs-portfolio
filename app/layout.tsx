bc955bc36cfaacf7effa123b4f7809316d12e4fc
Sadman Sakib <ssakib4040@gmail.com>
2024-02-10 01:02:24 +0600

    Sidebar mobile button position change with hover state

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@ import { Inter } from "next/font/google";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 
-import "./globals.css";
+import "./globals.scss";
 
 const inter = Inter({ subsets: ["latin"] });
 
