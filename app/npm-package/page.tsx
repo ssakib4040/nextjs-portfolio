658006822ff6703a1cd54394430920819731a97c
Sadman Sakib <ssakib4040@gmail.com>
2024-02-10 01:15:45 +0600

    placeholder added for npm package image

--- app/npm-package/page.tsx
+++ app/npm-package/page.tsx
@@ -70,9 +70,16 @@ export default function page() {
           >
             <Image
               src="/npm-placeholder.webp"
+              placeholder={`data:image/svg+xml;base64,${toBase64(
+                shimmer(700, 475)
+              )}`}
+              style={{
+                maxWidth: "100%",
+                height: "auto",
+              }}
               alt=""
-              width={1080}
-              height={480}
+              width={1600}
+              height={900}
             />
             <div className="p-6">
               <h2 className="text-xl font-bold text-gray-800">{npm.title}</h2>
@@ -139,3 +146,22 @@ export default function page() {
     </div>
   );
 }
+
+const shimmer = (w: number, h: number) => `
+<svg width="${w}" height="${h}" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+  <defs>
+    <linearGradient id="g">
+      <stop stop-color="#333" offset="20%" />
+      <stop stop-color="#222" offset="50%" />
+      <stop stop-color="#333" offset="70%" />
+    </linearGradient>
+  </defs>
+  <rect width="${w}" height="${h}" fill="#333" />
+  <rect id="r" width="${w}" height="${h}" fill="url(#g)" />
+  <animate xlink:href="#r" attributeName="x" from="-${w}" to="${w}" dur="1s" repeatCount="indefinite"  />
+</svg>`;
+
+const toBase64 = (str: string) =>
+  typeof window === "undefined"
+    ? Buffer.from(str).toString("base64")
+    : window.btoa(str);
