353a801b00337c92e28145e504f0392cc5ac4941
Sadman Sakib <ssakib4040@gmail.com>
2025-01-29 00:21:47 +0600

    loader added

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,5 +1,9 @@
 import React from "react";
 
 export default function loading() {
-  return <div>loading...</div>;
+  return (
+    <div className="h-screen w-full flex justify-center items-center">
+      <span className="loading loading-ball loading-lg text-[#0D6EFD]"></span>
+    </div>
+  );
 }
